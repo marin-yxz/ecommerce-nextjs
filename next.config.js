5b20160d090cbae8d7d37f0edcab1ddf7614ef00
marin-yxz <marin.grebenar.fax@gmail.com>
2022-06-13 21:12:53 +0200

    Fix styled component not server side rendering

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-
-module.exports = nextConfig;
-module.exports = {
   compiler: {
-    // ssr and displayName are configured by default
     styledComponents: true,
   },
 };
+
+module.exports = nextConfig;
