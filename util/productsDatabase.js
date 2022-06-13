9c18ad390c5544ca4e800ab81e26fef5dcf5637b
marin-yxz <marin.grebenar.fax@gmail.com>
2022-06-13 20:23:40 +0200

    Deployment adjustment

--- util/productsDatabase.js
+++ util/productsDatabase.js
@@ -1,21 +1,8 @@
 import camelcaseKeys from 'camelcase-keys';
 import { config } from 'dotenv-safe';
 import postgres from 'postgres';
+import setPostgresDefaultsOnHeroku from './setPosgtresDefaultOnHeroku';
 
-function setPostgresDefaultsOnHeroku() {
-  if (process.env.DATABASE_URL) {
-    const { parse } = require('pg-connection-string');
-
-    // Extract the connection information from the Heroku environment variable
-    const { host, database, user, password } = parse(process.env.DATABASE_URL);
-
-    // Set standard environment variables
-    process.env.PGHOST = host;
-    process.env.PGDATABASE = database;
-    process.env.PGUSERNAME = user;
-    process.env.PGPASSWORD = password;
-  }
-}
 setPostgresDefaultsOnHeroku();
 config();
 
